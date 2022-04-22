3ee9861f3ccd3001884f669efe3ab17ec0d49d26
Roman Latypov <jiggawhat5@gmail.com>
2022-04-23 02:40:59 +0500

    uploaded to github

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/App.css';
+import Header from "./Header";
+import Main from "./Main";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
